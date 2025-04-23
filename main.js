5f681d4c2672ddf25ea73d07910eb3e98a1790d1
Barrie Freyre <barriedirk@yahoo.com>
2025-04-23 00:36:06 -0500

    improve menu togle with js

---
+++ main.js
@@ -0,0 +1,17 @@
+import { $, $$ } from "./utils.js";
+
+(async () => {
+  // navigator
+  const $btnsShowMenu = $(".header__nav-show-menu");
+
+  $btnsShowMenu.addEventListener("click", function (evt) {
+    evt.preventDefault();
+
+    const isExpanded = this.getAttribute("aria-expanded") === "true";
+    const expandedValue = isExpanded ? "false" : "true";
+    const labelValue = isExpanded ? "Close toggle menu" : "Open toggle menu";
+
+    this.setAttribute("aria-expanded", expandedValue);
+    this.setAttribute("aria-expanded", expandedValue);
+  });
+})();
