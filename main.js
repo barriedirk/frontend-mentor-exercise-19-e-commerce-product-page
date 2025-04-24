086ef386f692a127b852928a4f46a43f926edcb1
Barrie Freyre <barriedirk@yahoo.com>
2025-04-23 22:59:15 -0500

    finish main changes for responsive design

--- main.js
+++ main.js
@@ -15,3 +15,36 @@ import { $, $$ } from "./utils.js";
     this.setAttribute("aria-expanded", expandedValue);
   });
 })();
+
+// @todo, adapt
+const images = [
+  { src: "./images/image-product-1.jpg", alt: "Product view 1" },
+  { src: "./images/image-product-2.jpg", alt: "Product view 2" },
+  { src: "./images/image-product-3.jpg", alt: "Product view 3" },
+  { src: "./images/image-product-4.jpg", alt: "Product view 4" },
+];
+
+let currentIndex = 0;
+const mainImage = document.getElementById("product__main-image");
+
+function updateImage() {
+  mainImage.src = images[currentIndex].src;
+  mainImage.alt = images[currentIndex].alt;
+}
+
+function showNextSlide() {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateImage();
+}
+
+function showPrevSlide() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  updateImage();
+}
+
+function setImage(index) {
+  currentIndex = index;
+  updateImage();
+}
+
+updateImage();
